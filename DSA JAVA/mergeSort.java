c8971c74f616cb34daccb2e21d41a58c0bea0c23
Trisha Verma <128292311+trishave@users.noreply.github.com>
2024-01-30 23:45:21 +0530

    Add files via upload

---
+++ DSA JAVA/mergeSort.java
@@ -0,0 +1,97 @@
+import java.util.*;
+
+public class mergeSort {
+
+ 
+
+    public static void mergeSort(int[] arr, int l, int r){
+
+       if(l >= r) {
+
+           return;
+
+       }
+
+ 
+
+     int  mid = (l+r)/2;
+
+       mergeSort(arr, l, mid);
+
+       mergeSort(arr, mid+1, r);
+
+       merge(arr, l, mid,r);
+
+ 
+
+    }
+
+ 
+
+   public static void merge(int[] arr, int low, int mid, int high)
+
+    {
+
+        ArrayList<Integer> temp = new ArrayList<>(); 
+
+ 
+
+         int left = low;
+
+         int right = mid+1;
+
+         while(left<= mid && right<= high){
+
+             if (arr[left] <= arr[right]) {
+
+                temp.add(arr[left]);
+
+                left++;
+
+            } else {
+
+                temp.add(arr[right]);
+
+                right++;
+
+            }
+
+         }
+
+ 
+
+         while(left<= mid){
+
+               temp.add(arr[left]);
+
+                left++;
+
+         }
+
+         while(right<= high){
+
+               temp.add(arr[right]);
+
+                right++;
+
+         }
+
+ 
+
+         for (int i = low; i <= high; i++) {
+
+            arr[i] = temp.get(i - low);
+
+        }
+
+    }
+
+ 
+
+  
+
+}
+
+ 
+
+
