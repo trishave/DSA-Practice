d2cb0d991cd0327fcb49b3ecdcb1d6848946f18c
Trisha Verma <128292311+trishave@users.noreply.github.com>
2024-02-02 18:56:37 +0530

    left rotate the array by one.

---
+++ DSA JAVA/rotate1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class rotate1 {
+static void solve(int arr[], int n) {
+  int temp[]=new int[n];
+  for (int i = 1; i < n; i++) {
+    temp[i - 1] = arr[i];
+  }
+  temp[n - 1] = arr[0];
+  for (int i = 0; i < n; i++) {
+    System.out.print(temp[i]+" ");
+  }
+  
+}
+public static void main(String args[]) {
+  int n=5;
+  
+  int arr[]= {1,2,3,4,5};
+  solve(arr, n);
+}
+}
