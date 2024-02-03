69039871548505c0d31315fa67f400f29343d25a
Trisha Verma <128292311+trishave@users.noreply.github.com>
2024-02-04 00:41:40 +0530

    Kadane's algorithm

---
+++ DSA JAVA/kadaneAlgo.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+
+public class kadaneAlgo {
+    public static int maxSubarraySum(int[] arr, int n) {
+        int maxi = Integer.MIN_VALUE; // maximum sum
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                // subarray = arr[i.....j]
+                int sum = 0;
+
+                //add all the elements of subarray:
+                for (int k = i; k <= j; k++) {
+                    sum += arr[k];
+                }
+
+                maxi = Math.max(maxi, sum);
+            }
+        }
+
+        return maxi;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int n = arr.length;
+        int maxSum = maxSubarraySum(arr, n);
+        System.out.println("The maximum subarray sum is: " + maxSum);
+
+    }
+
+}
+
